68915e5554eefb4ae625f863019557998c35bd10
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2020-02-02 17:04:16 +0300

    handle dice clicks

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,39 +18,103 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
-  var leftDiceNum = 5;
+class DicePage extends StatefulWidget {
+  DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNum = 1;
+  var rightDiceNum = 2;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
-        children: <Widget>[leftButton(leftDiceNum), rightButton()],
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+                padding: EdgeInsets.all(20),
+                child: FlatButton(onPressed: onPressed(),
+                    child: Image.asset('images/dice$leftDiceNum.png'))),
+          ),
+          Expanded(
+            child: Padding(
+                padding: EdgeInsets.all(20),
+                child: FlatButton(
+                    onPressed: onPressed(),
+                    child: Image.asset('images/dice$rightDiceNum.png'))),
+          )
+        ],
       ),
     );
   }
 
-  Expanded rightButton() {
-    return Expanded(
-      child: Padding(
-          padding: EdgeInsets.all(20),
-          child: FlatButton(
-              onPressed: () {
-                print('Hello from right button');
-              },
-              child: Image.asset('images/dice2.png'))),
-    );
-  }
-
-  Expanded leftButton(int leftDiceNum) {
-    return Expanded(
-      child: Padding(
-          padding: EdgeInsets.all(20),
-          child: FlatButton(
-              onPressed: () {
-                print('Hello from left button');
-              },
-              child: Image.asset('images/dice$leftDiceNum.png'))),
-    );
+  Function onPressed() {
+    return () {
+      setState(() {
+        leftDiceNum = Random().nextInt(6) + 1;
+        rightDiceNum = Random().nextInt(6) + 1;
+      });
+      print('Hello from left button');
+    };
   }
 }
+
+// class DicePage extends StatelessWidget {
+//   var leftDiceNum = 5;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Row(
+//         children: <Widget>[
+//           Expanded(
+//             child: Padding(
+//                 padding: EdgeInsets.all(20),
+//                 child: FlatButton(
+//                     onPressed: () {
+//                       print('Hello from left button');
+//                     },
+//                     child: Image.asset('images/dice$leftDiceNum.png'))),
+//           ),
+//           Expanded(
+//             child: Padding(
+//                 padding: EdgeInsets.all(20),
+//                 child: FlatButton(
+//                     onPressed: () {
+//                       print('Hello from right button');
+//                     },
+//                     child: Image.asset('images/dice2.png'))),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+
+//   Expanded rightButton() {
+//     return Expanded(
+//       child: Padding(
+//           padding: EdgeInsets.all(20),
+//           child: FlatButton(
+//               onPressed: () {
+//                 print('Hello from right button');
+//               },
+//               child: Image.asset('images/dice2.png'))),
+//     );
+//   }
+
+//   Expanded leftButton(int leftDiceNum) {
+//     return Expanded(
+//       child: Padding(
+//           padding: EdgeInsets.all(20),
+//           child: FlatButton(
+//               onPressed: () {
+//                 print('Hello from left button');
+//               },
+//               child: Image.asset('images/dice$leftDiceNum.png'))),
+//     );
+//   }
+// }
